e4fb7573aaadf33d7a419c3a1a895baf128476fe
Araika Singh <roseymods@gmail.com>
2022-03-18 12:44:24 +0530

    Time: 4 ms (91.77%), Space: 7.2 MB (86.15%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,62 +1,45 @@
 class Solution {
 public:
-    bool isSafe(vector<string>& board, int n, int row, int col){
-        int row1 = row;
-        int col1 = col;
+    void helper(vector<vector<string>>& res, vector<string>& board, vector<bool>& rowhash, vector<bool>& primarydiaghash, vector<bool>& otherdiaghash, int n, int col){
         
-        while(row >=0 && col >=0){
-            if(board[row][col] == 'Q')
-                return false;
-            row--;
-            col--;
-        }
-        
-        row = row1;
-        col = col1;
-        
-        while(col >= 0){
-            if(board[row][col]  == 'Q')
-                return false;
-            col--;
-        }
-        
-        col = col1;
-        
-        while(row < n && col >= 0){
-            if(board[row][col]  == 'Q')
-                return false;
-            row++;
-            col--;
-        }
-        
-        return true;
-    }
-    
-    void helper(vector<vector<string>>& res, vector<string>& board, int n, int col){
         if(col == n){
             res.push_back(board);
             return;
         }
         
         for(int row=0; row<n; row++){
-            if(isSafe(board, n, row, col)){
+            if(!rowhash[row] && !primarydiaghash[n-1+col-row] && !otherdiaghash[row+col]){
+                
                 board[row][col] = 'Q';
-                helper(res, board, n, col+1);
+                rowhash[row] = true;
+                primarydiaghash[n-1+col-row] = true;
+                otherdiaghash[row+col] = true;
+                
+                helper(res, board, rowhash, primarydiaghash, otherdiaghash, n, col+1);
+                
                 board[row][col] = '.';
+                rowhash[row] = false;
+                primarydiaghash[n-1+col-row] = false;
+                otherdiaghash[row+col] = false;
             }
         }
     }
     
     vector<vector<string>> solveNQueens(int n) {
+        
+        vector<bool> rowhash(n, false);
+        vector<bool> primarydiaghash(2*n-1, false);
+        vector<bool> otherdiaghash(2*n-1, false);
+        
         vector<vector<string>> res;
         vector<string> board(n);
         string s(n, '.');
         
-        for(int i=0; i<n; i++){
+        for(int i=0;i<n;i++){
             board[i] = s;
         }
         
-        helper(res, board, n, 0);
+        helper(res, board, rowhash, primarydiaghash, otherdiaghash, n, 0);
         return res;
     }
 };

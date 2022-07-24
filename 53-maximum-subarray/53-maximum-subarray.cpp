33015e052187a80c5373a0261719c921a9d3466e
Araika Singh <roseymods@gmail.com>
2022-07-24 14:46:49 +0530

    Time: 143 ms (69.37%), Space: 67.8 MB (53.44%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int max_so_far = INT_MIN; // also handles the single largest values
-        int curr_max = 0; // handles the breaking of sums after contiguous ones
+        int maxSoFar = -100000;
+        int currMax = nums[0];
         
-        for(auto num : nums){
-            curr_max = num + curr_max;
-            if(num > curr_max)
-                curr_max = num;
-            
-            if(curr_max > max_so_far)
-                max_so_far = curr_max;
+        for(int i=1; i<nums.size(); i++){
+            maxSoFar = max(maxSoFar, currMax);
+            currMax = max(currMax+nums[i], nums[i]);
         }
         
-        return max_so_far;
+        return max(maxSoFar, currMax);
     }
 };
